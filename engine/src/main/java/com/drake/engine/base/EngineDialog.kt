fd365dabfe59bdc5c66aba125bcf0e2f8d212d67
drake <liangjinggpx@gmail.com>
2020-09-10 23:11:12 +0800

    update

--- engine/src/main/java/com/drake/engine/base/EngineDialog.kt
+++ engine/src/main/java/com/drake/engine/base/EngineDialog.kt
@@ -1,8 +1,17 @@
 /*
- * Copyright (C) 2018, Umbrella CompanyLimited All rights reserved.
- * Project：Engine
- * Author：Drake
- * Date：9/14/19 10:46 AM
+ * Copyright (C) 2018 Drake, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.drake.engine.base
@@ -12,8 +21,8 @@ import android.content.Context
 import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import com.drake.engine.databinding.inflate
 
 abstract class EngineDialog<B : ViewDataBinding>(context: Context) : Dialog(context),
     OnClickListener {
@@ -28,7 +37,7 @@ abstract class EngineDialog<B : ViewDataBinding>(context: Context) : Dialog(cont
     protected abstract fun initData()
 
     override fun setContentView(layoutResID: Int) {
-        binding = context.inflate(layoutResID)
+        binding = DataBindingUtil.inflate(layoutInflater, layoutResID, null, false)
         setContentView(binding.root)
         init()
     }
--- engine/src/test/java/com/drake/engine/ExampleUnitTest.java
+++ engine/src/test/java/com/drake/engine/ExampleUnitTest.java
@@ -1,8 +1,17 @@
 /*
- * Copyright (C) 2018,银人网络科技（长沙）有限公司 All rights reserved.
- * Project：YiFuKeCheng
- * Author：设计师吴彦祖
- * Date：18-4-12 下午11:33
+ * Copyright (C) 2018 Drake, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.drake.engine;
