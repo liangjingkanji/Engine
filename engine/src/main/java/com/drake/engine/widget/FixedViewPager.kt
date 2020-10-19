716437ca3b45c2228311db2b8d65bdb5ddedd575
drake <liangjinggpx@gmail.com>
2020-10-19 16:04:12 +0800

    FixedViewPager.kt 关闭划动和修复崩溃

--- engine/src/main/java/com/drake/engine/widget/FixedViewPager.kt
+++ engine/src/main/java/com/drake/engine/widget/FixedViewPager.kt
@@ -4,14 +4,23 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.viewpager.widget.ViewPager
+import com.drake.engine.R
 
 /**
  * 解决 IllegalArgumentException: pointerIndex out of range
+ *
+ * @property scrollable 是否支持划动 属性设置 [app:pager_scrollable]
  */
-class FixedViewPager @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null
-) : ViewPager(context, attrs) {
+class FixedViewPager : ViewPager {
+
+    var scrollable: Boolean = true
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs) {
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.FixedViewPager)
+        scrollable = attributes.getBoolean(R.styleable.FixedViewPager_pager_scrollable, true)
+        attributes.recycle()
+    }
 
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
         var isIntercept = false
@@ -20,15 +29,16 @@ class FixedViewPager @JvmOverloads constructor(
         } catch (e: Exception) {
             e.printStackTrace()
         }
-        return isIntercept
+        return isIntercept && scrollable
     }
 
     override fun onTouchEvent(ev: MotionEvent): Boolean {
+        var consume = false
         try {
-            super.onTouchEvent(ev)
+            consume = super.onTouchEvent(ev)
         } catch (e: Exception) {
             e.printStackTrace()
         }
-        return false
+        return consume && scrollable
     }
 }
