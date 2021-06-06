c35a99ca7404907a64145f315d189e111123318a
drake <liangjinggpx@gmail.com>
2021-06-07 01:26:02 +0800

    修复FixedViewPager滑动失效

--- engine/src/main/java/com/drake/engine/widget/FixedViewPager.kt
+++ engine/src/main/java/com/drake/engine/widget/FixedViewPager.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2018 Drake, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package com.drake.engine.widget
 
 import android.content.Context
@@ -8,37 +26,37 @@ import com.drake.engine.R
 
 /**
  * 解决 IllegalArgumentException: pointerIndex out of range
- *
- * @property scrollable 是否支持划动 属性设置 [app:pager_scrollable]
  */
-class FixedViewPager : ViewPager {
+class FixedViewPager(context: Context, attrs: AttributeSet? = null) : ViewPager(context, attrs) {
 
+    /** 是否支持划动 属性设置 [app:pager_scrollable] */
     var scrollable: Boolean = true
 
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs) {
+    init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.FixedViewPager)
         scrollable = attributes.getBoolean(R.styleable.FixedViewPager_pager_scrollable, true)
         attributes.recycle()
     }
 
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
-        var isIntercept = scrollable
-        try {
-            isIntercept = super.onInterceptTouchEvent(ev)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        if (scrollable) {
+            try {
+                return super.onInterceptTouchEvent(ev)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
-        return isIntercept && scrollable
+        return false
     }
 
     override fun onTouchEvent(ev: MotionEvent): Boolean {
-        var consume = scrollable
-        try {
-            consume = super.onTouchEvent(ev)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        if (scrollable) {
+            try {
+                return super.onTouchEvent(ev)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
-        return consume && scrollable
+        return false
     }
 }
