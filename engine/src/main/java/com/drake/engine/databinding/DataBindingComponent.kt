1ac9e97c234696f375123714d8deb13863143c1d
drake <liangjinggpx@gmail.com>
2021-09-17 18:57:18 +0800

    DataBinding属性

--- engine/src/main/java/com/drake/engine/databinding/DataBindingComponent.kt
+++ engine/src/main/java/com/drake/engine/databinding/DataBindingComponent.kt
@@ -21,9 +21,12 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContextWrapper
 import android.content.res.ColorStateList
+import android.graphics.Paint
 import android.os.Build
+import android.text.TextUtils
 import android.view.View
 import android.view.View.NO_ID
+import android.webkit.WebView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -32,25 +35,15 @@ import androidx.databinding.BindingAdapter
 import com.drake.engine.base.app
 import com.drake.engine.utils.throttleClick
 import com.google.android.material.button.MaterialButton
+import java.math.RoundingMode
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 object DataBindingComponent {
 
-    /**
-     * 在绑定视图时可以用于Model来处理UI, 由于破坏视图和逻辑解耦的规则不是很建议使用
-     * 这会导致不方便业务逻辑进行单元测试
-     *
-     * @see OnBindListener 该接口支持泛型定义具体视图
-     *
-     * @receiver View
-     * @param listener OnBindListener<View>
-     */
-    @BindingAdapter("onBind")
-    @JvmStatic
-    fun View.setBindListener(listener: OnBindListener) {
-        listener.onBind(this)
-    }
-
+    //<editor-fold desc="间距">
     @BindingAdapter("paddingStart", "paddingEnd", requireAll = false)
     @JvmStatic
     fun View.setPaddingRtl(start: View?, end: View?) {
@@ -60,6 +53,7 @@ object DataBindingComponent {
             setPaddingRelative(startFinal, paddingTop, endFinal, paddingBottom)
         }
     }
+    //</editor-fold>
 
     // <editor-fold desc="图片">
 
@@ -225,7 +219,7 @@ object DataBindingComponent {
     @SuppressLint("CheckResult")
     @BindingAdapter("click")
     @JvmStatic
-    fun View.setPreventClickListener(onClickListener: View.OnClickListener?) {
+    fun View.setThrottleClickListener(onClickListener: View.OnClickListener?) {
         if (onClickListener != null) {
             throttleClick { onClickListener.onClick(this) }
         }
@@ -234,22 +228,21 @@ object DataBindingComponent {
 
     /**
      * 自动将点击事件映射到Activity上
-     *
-     * @param isPrevent 是否只支持快速点击
+     * @param throttle 是否只支持快速点击
      */
     @SuppressLint("CheckResult")
     @BindingAdapter("hit")
     @JvmStatic
-    fun View.hit(isPrevent: Boolean = true) {
+    fun View.hit(throttle: Boolean = true) {
         var context = context
-
         while (context is ContextWrapper) {
             if (context is View.OnClickListener) {
                 val clickListener = context as View.OnClickListener
-
-                if (isPrevent) {
+                if (throttle) {
                     throttleClick { clickListener.onClick(this) }
-                } else setOnClickListener(clickListener)
+                } else {
+                    setOnClickListener(clickListener)
+                }
             }
             context = context.baseContext
         }
@@ -290,13 +283,161 @@ object DataBindingComponent {
     // </editor-fold>
 
 
+    // <editor-fold desc="货币">
+
+    @SuppressLint("SetTextI18n")
+    @BindingAdapter("rmb")
+    @JvmStatic
+    fun TextView.formatCNY(number: String?) {
+        if (!number.isNullOrEmpty()) {
+            val format = "¥${number.format()}"
+            if (format != text.toString()) text = format
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    @BindingAdapter("rmb")
+    @JvmStatic
+    fun TextView.formatCNY(number: Double) {
+        val numberFormat = NumberFormat.getInstance()
+        numberFormat.minimumFractionDigits = 2
+        numberFormat.maximumFractionDigits = 2
+        numberFormat.roundingMode = RoundingMode.UP
+        val format = "¥${numberFormat.format(number ?: 0.0)}"
+        if (format != text.toString()) text = format
+    }
+
+    /**
+     * 设置rmb，已经除100
+     */
+    @SuppressLint("SetTextI18n")
+    @BindingAdapter("rmb")
+    @JvmStatic
+    fun TextView.formatCNY(number: Long) {
+        /**
+         * 默认看做 "分" 处理(除以100)
+         */
+        val numberFormat = NumberFormat.getInstance()
+        numberFormat.minimumFractionDigits = 2
+        numberFormat.maximumFractionDigits = 2
+        numberFormat.roundingMode = RoundingMode.UP
+        val format = "¥${numberFormat.format(number / 100.0)}"
+        if (format != text.toString()) text = format
+    }
+
+    // </editor-fold>
+
+    // <editor-fold desc="时间">
+
+    @BindingAdapter(value = ["dateMilli", "dateFormat"], requireAll = false)
+    @JvmStatic
+    fun TextView.setDateFromMillis(milli: Long, format: String? = "yyyy-MM-dd") {
+        /**
+         * 格式化毫秒
+         */
+        val finalFormat = if (format.isNullOrBlank()) "yyyy-MM-dd" else format
+        val date = Date(milli)
+        val sf = SimpleDateFormat(finalFormat, Locale.CHINA)
+        val formatText = sf.format(date)
+        if (milli == 0L || formatText == text.toString()) {
+            return
+        }
+        text = formatText
+    }
+
+
+    /**
+     * 根据毫秒值来显示时间
+     */
+    @BindingAdapter(value = ["dateMilli", "dateFormat"], requireAll = false)
+    @JvmStatic
+    fun TextView.setDateFromMillis(milli: String?, format: String? = "yyyy-MM-dd") {
+        var formatText = ""
+        val finalFormat = if (format.isNullOrBlank()) "yyyy-MM-dd" else format
+        val date = Date(java.lang.Long.parseLong(formatText))
+        val sf = SimpleDateFormat(finalFormat, Locale.CHINA)
+        formatText = sf.format(date)
+        if (milli.isNullOrEmpty() || formatText == text.toString()) {
+            return
+        }
+        text = formatText
+    }
+
+    @BindingAdapter(value = ["dateSecond", "dateFormat"], requireAll = false)
+    @JvmStatic
+    fun TextView.setDateFromSecond(second: Long, format: String? = "yyyy-MM-dd") {
+        /**
+         * 格式化毫秒
+         */
+        val finalFormat = if (format.isNullOrBlank()) "yyyy-MM-dd" else format
+        val date = Date(second * 1000)
+        val sf = SimpleDateFormat(finalFormat, Locale.CHINA)
+        val formatText = sf.format(date)
+        if (second == 0L || formatText == text.toString()) {
+            return
+        }
+        text = formatText
+    }
+
+    @BindingAdapter(value = ["dateSecond", "dateFormat"], requireAll = false)
+    @JvmStatic
+    fun TextView.setDateFromSecond(second: String, format: String? = "yyyy-MM-dd") {
+        /**
+         * 格式化毫秒
+         */
+        val finalFormat = if (format.isNullOrBlank()) "yyyy-MM-dd" else format
+        val date = Date(java.lang.Long.parseLong(second) * 1000)
+        val sf = SimpleDateFormat(finalFormat, Locale.CHINA)
+        val formatText = sf.format(date)
+        if (TextUtils.isEmpty(second) || formatText == text.toString()) {
+            return
+        }
+        text = formatText
+    }
+
+    // </editor-fold>
+
+    //<editor-fold desc="字符串">
+    @BindingAdapter("del")
+    @JvmStatic
+    fun TextView.setDel(isAdd: Boolean) {
+        if (isAdd) {
+            paint.flags = Paint.STRIKE_THRU_TEXT_FLAG or Paint.ANTI_ALIAS_FLAG   // 设置中划线并加清晰
+        }
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="网页">
+    @BindingAdapter("url")
+    @JvmStatic
+    fun WebView.setUrl(url: String?) {
+        if (!url.isNullOrEmpty()) {
+            loadDataWithBaseURL(null, url, "text/html", "UTF-8", null)
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="回调">
     /**
      * 在绑定视图时可以用于Model来处理UI, 由于破坏视图和逻辑解耦的规则不是很建议使用
-     * 这会导致不方便业务逻辑进行单元测试
      *
+     * @see OnBindListener 该接口支持泛型定义具体视图
+     *
+     * @receiver View
+     * @param listener OnBindListener<View>
+     */
+    @BindingAdapter("onBind")
+    @JvmStatic
+    fun View.setOnBindListener(listener: OnBindListener) {
+        listener.onBind(this)
+    }
+
+    /**
+     * 在绑定视图时可以用于Model来处理UI, 由于破坏视图和逻辑解耦的规则不是很建议使用
      */
     interface OnBindListener {
-
         fun onBind(v: View)
     }
+    //</editor-fold>
 }
